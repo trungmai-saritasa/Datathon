68d9d702d6cc378c58d289141a86e915eac7482a
Trung Mai <trung.mai@saritasa.com>
2023-07-02 12:17:18 +0700

    Final 1

--- app_build/Home.py
+++ app_build/Home.py
@@ -1,71 +1,35 @@
-# App streamlit to deploy the data analysis:
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
 
-if "df" or "df_non_tech" not in st.session_state:
-    st.session_state.df = None
-    st.session_state.df_non_tech = None
+# Define valid credentials for manager and employee
+valid_credentials = {
+    "manager": {"username": "manager", "password": "manager123"},
+    "employee": {"username": "employee", "password": "employee123"}
+}
 
-#-------------------------------------------------------
-# App title:
-col1, col2, col3 = st.columns([1,6,1])
+# Define Streamlit app
+def main():
+    st.title("Login Example")
 
-with col1:
-    st.write("")
+    # Sidebar
+    st.sidebar.title("Login")
+    role = st.sidebar.selectbox("Role", ("manager", "employee"))
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+    login_button = st.sidebar.button("Login")
 
-with col2:
-    st.image('https://storage.googleapis.com/kaggle-competitions/kaggle/39462/logos/header.png?t=2022-09-30-14-50-31')
-    st.markdown("<h1 style='text-align: center; color: #B799FF;'>ANALYSIS OF ML/DS JOBS</h1>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center; color: #AEE2FF;'>Vietnam and the World 2019 - 2022</h4>", unsafe_allow_html=True)
+    # Check if login button is clicked
+    if login_button:
+        if username == valid_credentials[role]["username"] and password == valid_credentials[role]["password"]:
+            st.success(f"Logged in as {role}")
+            if role == "manager":
+                # Show manager content
+                st.write("Welcome, Manager!")
+            else:
+                # Show employee content
+                st.write("Welcome, Employee!")
+        else:
+            st.error("Invalid username or password")
 
-with col3:
-    st.write("")    
-
-
-st.markdown("---", unsafe_allow_html=True)
-
-#-------------------------------------------------------
-st.subheader("1. Thông tin về dữ liệu:")
-
-col1, col2, col3 = st.columns([1,6,1])
-with col1:
-    st.write("")
-with col2:
-    st.image('https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210301191415/Top-Kaggle-Courses-for-Data-Science.png')
-with col3:
-    st.write("")   
-
-
-st.info("Bộ dữ liệu được lấy từ Kaggle, ở cuộc thi phân tích: (2019 - 2022) Kaggle Machine Learning & Data Science Survey (https://www.kaggle.com/competitions/kaggle-survey-2022/overview). \
-        Bộ dữ liệu được Kaggle khảo sát từ người dùng để đưa ra những góc nhìn toàn diện về tình trạng của các ngành Khoa học dữ liệu và học máy. Cuộc khảo sát được thực hiện từ năm 2019 đến năm 2022. Những người khảo sát đến từ các quốc gia khác nhau, trong đó có Việt Nam.", icon="🔥")
-
-# df = pd.read_csv('analysis_df_employee.csv')
-df = pd.read_csv('app_build/analysis_df_employee.csv')
-st.dataframe(df)
-
-st.info("Ngoài bộ dữ liệu được khảo sát, nhóm còn tìm thêm một bộ dữ liệu con về các vai trò công việc trong lĩnh vực DS/ML với mức lương cơ bản của nó trong khoảng từ năm 2020 - 2021 để bổ sung phân tích cho toàn bộ bài làm của nhóm. Được lấy trên trang: https://www.kaggle.com/datasets/ruchi798/data-science-job-salaries", icon="🔥")
-# df_title_job = pd.read_csv('analysis_title_salary.csv')
-df_title_job = pd.read_csv('app_build/analysis_title_salary.csv')
-
-st.dataframe(df_title_job)
-
-#-------------------------------------------------------
-st.subheader("2. Thông tin về ứng dụng:")
-st.write("""**🧑‍💻️ Tên ứng dụng**: Web app phân tích tình hình việc làm ngành Khoa học dữ liệu và học máy. \n
-**👦Người thực hiện**: Nhóm 2 - Trực quan hóa dữ liệu. \n
-**💻Ngôn ngữ sử dụng để trực quan**: Python. \n
-**📱Framework sử dụng để triển khai**: Streamlit. \n
-**📋 Chức năng của ứng dụng**: \n
-- Cung cấp các biểu đồ toàn diện cũng như các thông tin về ngành.
-- Cung cấp các thông số, biểu đồ thống kê mô tả.
-- Cung cấp các phân tích dữ liệu đơn giản.
-- Phân tích hồi quy, giải thích và dự đoán mô hình. """)
-
-# df_non_tech = pd.read_csv("analysis_df.csv")
-df_non_tech = pd.read_csv("app_build/analysis_df.csv")
-
-st.session_state.df = df
-st.session_state.df_non_tech = df_non_tech
-st.session_state.df_title_job = df_title_job
+# Run the app
+if __name__ == "__main__":
+    main()
---
+++ app_build/pages/Manager.py
@@ -0,0 +1,361 @@
+import os
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from dotenv import load_dotenv
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+
+# Config trang -----------------------------------------
+st.set_page_config(
+    page_title="Dashboard Data Analysis",
+    page_icon="📊",
+    layout="wide"
+)
+
+df = pd.read_excel("app_build/MDSInc_sales.xlsx")
+df["order_date"] = pd.to_datetime(df["order_date"])
+
+col1, col2, col3 = st.columns([1,6,1])
+
+with col1:
+    st.write("")
+with col2:
+    st.markdown("<h1 style='text-align: center; color: #B799FF;'>MANAGER DASHBOARD</h1>", unsafe_allow_html=True)
+    st.markdown("---", unsafe_allow_html=True)
+with col3:
+    st.write("")
+
+# plot 0
+st.markdown("##### Bộ lọc năm toàn cục:", unsafe_allow_html=True)
+
+year_filter = st.selectbox("Lọc theo năm:", df['order_date'].dt.year.unique())
+df_year = df[df["order_date"].dt.year == year_filter]
+
+# Tính năm trước với năm đã được chọn:
+if year_filter == 2011:
+    pre_year = 2011
+else:
+    pre_year = year_filter - 1
+
+df_pre_year = df[df["order_date"].dt.year == pre_year]
+
+# Lấy các độ đo thống kê:
+df_describe = df_year.describe().round(2).T
+df_describe_pre = df_pre_year.describe().round(2).T
+
+st.markdown("#### 2. Thông số thống kê về Profit ($):")
+tab1, tab2, tab3 = st.tabs(["Profit", "Sales", "Quantity"])
+with tab1:
+    stats_age_col1, _ = st.columns(2)
+    stats_age_col1.metric(
+        label="Sum",
+        value=df_year["profit"].sum(),
+        delta=round(float(df_year["profit"].sum() - df_pre_year["profit"].sum()), 2),
+    )
+with tab2:
+    stats_age_col1, _ = st.columns(2)
+    stats_age_col1.metric(
+        label="Sum",
+        value=df_year["sales"].sum(),
+        delta=round(float(df_year["sales"].sum() - df_pre_year["sales"].sum()), 2),
+    )
+with tab3:
+    stats_age_col1, _ = st.columns(2)
+    stats_age_col1.metric(
+        label="Sum",
+        value=df_year["quantity"].sum(),
+        delta=round(float(df_year["quantity"].sum() - df_pre_year["quantity"].sum()), 2),
+    )
+
+
+tab1, tab2, tab3 = st.tabs(["Profit", "Sales", "Quantity"])
+# Tạo các cột để hiển thị thông số thống kê (P1):
+with tab1:
+    stats_age_col1, stats_age_col2, stats_age_col3, stats_age_col4 = st.columns(4)
+    stats_age_col1.metric(
+        label="Mean",
+        value=df_describe.loc["profit"].loc["mean"],
+        delta=round(df_describe.loc["profit"].loc["mean"] - df_describe_pre.loc["profit"].loc["mean"], 2),
+    )
+    stats_age_col2.metric(
+        label="Median",
+        value=df_describe.loc["profit"].loc["50%"],
+        delta=round(df_describe.loc["profit"].loc["50%"] - df_describe_pre.loc["profit"].loc["50%"], 2),
+    )
+    stats_age_col3.metric(
+        label="Std",
+        value=df_describe.loc["profit"].loc["std"],
+        delta=round(df_describe.loc["profit"].loc["std"] - df_describe_pre.loc["profit"].loc["std"], 2),
+    )
+    stats_age_col4.metric(
+        label="Count",
+        value=df_describe.loc["profit"].loc["count"].astype(int),
+        delta=round(df_describe.loc["profit"].loc["count"] - df_describe_pre.loc["profit"].loc["count"], 2),
+    )
+
+    stats_age_col5, stats_age_col6, stats_age_col7, stats_age_col8 = st.columns(4)
+    stats_age_col5.metric(
+        label="25%",
+        value=df_describe.loc["profit"].loc["25%"],
+        delta=round(df_describe.loc["profit"].loc["25%"] - df_describe_pre.loc["profit"].loc["25%"], 2),
+    )
+    stats_age_col6.metric(
+        label="75%",
+        value=df_describe.loc["profit"].loc["max"],
+        delta=round(df_describe.loc["profit"].loc["max"] - df_describe_pre.loc["profit"].loc["max"], 2),
+    )
+    stats_age_col7.metric(
+        label="Min",
+        value=df_describe.loc["profit"].loc["min"],
+        delta=round(df_describe.loc["profit"].loc["min"] - df_describe_pre.loc["profit"].loc["min"], 2),
+    )
+    stats_age_col8.metric(
+        label="Max",
+        value=df_describe.loc["profit"].loc["max"],
+        delta=round(df_describe.loc["profit"].loc["max"] - df_describe_pre.loc["profit"].loc["max"], 2),
+    )
+with tab2:
+    stats_age_col1, stats_age_col2, stats_age_col3, stats_age_col4 = st.columns(4)
+    stats_age_col1.metric(
+        label="Mean",
+        value=df_describe.loc["sales"].loc["mean"],
+        delta=round(df_describe.loc["sales"].loc["mean"] - df_describe_pre.loc["sales"].loc["mean"], 2),
+    )
+    stats_age_col2.metric(
+        label="Median",
+        value=df_describe.loc["sales"].loc["50%"],
+        delta=round(df_describe.loc["sales"].loc["50%"] - df_describe_pre.loc["sales"].loc["50%"], 2),
+    )
+    stats_age_col3.metric(
+        label="Std",
+        value=df_describe.loc["sales"].loc["std"],
+        delta=round(df_describe.loc["sales"].loc["std"] - df_describe_pre.loc["sales"].loc["std"], 2),
+    )
+    stats_age_col4.metric(
+        label="Count",
+        value=df_describe.loc["sales"].loc["count"].astype(int),
+        delta=round(df_describe.loc["sales"].loc["count"] - df_describe_pre.loc["sales"].loc["count"], 2),
+    )
+
+    stats_age_col5, stats_age_col6, stats_age_col7, stats_age_col8 = st.columns(4)
+    stats_age_col5.metric(
+        label="25%",
+        value=df_describe.loc["sales"].loc["25%"],
+        delta=round(df_describe.loc["sales"].loc["25%"] - df_describe_pre.loc["sales"].loc["25%"], 2),
+    )
+    stats_age_col6.metric(
+        label="75%",
+        value=df_describe.loc["sales"].loc["max"],
+        delta=round(df_describe.loc["sales"].loc["max"] - df_describe_pre.loc["sales"].loc["max"], 2),
+    )
+    stats_age_col7.metric(
+        label="Min",
+        value=df_describe.loc["sales"].loc["min"],
+        delta=round(df_describe.loc["sales"].loc["min"] - df_describe_pre.loc["sales"].loc["min"], 2),
+    )
+    stats_age_col8.metric(
+        label="Max",
+        value=df_describe.loc["sales"].loc["max"],
+        delta=round(df_describe.loc["sales"].loc["max"] - df_describe_pre.loc["sales"].loc["max"], 2),
+    )
+with tab3:
+    stats_age_col1, stats_age_col2, stats_age_col3, stats_age_col4 = st.columns(4)
+    stats_age_col1.metric(
+        label="Mean",
+        value=df_describe.loc["quantity"].loc["mean"],
+        delta=round(df_describe.loc["quantity"].loc["mean"] - df_describe_pre.loc["quantity"].loc["mean"], 2),
+    )
+    stats_age_col2.metric(
+        label="Median",
+        value=df_describe.loc["quantity"].loc["50%"],
+        delta=round(df_describe.loc["quantity"].loc["50%"] - df_describe_pre.loc["quantity"].loc["50%"], 2),
+    )
+    stats_age_col3.metric(
+        label="Std",
+        value=df_describe.loc["quantity"].loc["std"],
+        delta=round(df_describe.loc["quantity"].loc["std"] - df_describe_pre.loc["quantity"].loc["std"], 2),
+    )
+    stats_age_col4.metric(
+        label="Count",
+        value=df_describe.loc["quantity"].loc["count"].astype(int),
+        delta=round(df_describe.loc["quantity"].loc["count"] - df_describe_pre.loc["quantity"].loc["count"], 2),
+    )
+
+    stats_age_col5, stats_age_col6, stats_age_col7, stats_age_col8 = st.columns(4)
+    stats_age_col5.metric(
+        label="25%",
+        value=df_describe.loc["quantity"].loc["25%"],
+        delta=round(df_describe.loc["quantity"].loc["25%"] - df_describe_pre.loc["quantity"].loc["25%"], 2),
+    )
+    stats_age_col6.metric(
+        label="75%",
+        value=df_describe.loc["quantity"].loc["max"],
+        delta=round(df_describe.loc["quantity"].loc["max"] - df_describe_pre.loc["quantity"].loc["max"], 2),
+    )
+    stats_age_col7.metric(
+        label="Min",
+        value=df_describe.loc["quantity"].loc["min"],
+        delta=round(df_describe.loc["quantity"].loc["min"] - df_describe_pre.loc["quantity"].loc["min"], 2),
+    )
+    stats_age_col8.metric(
+        label="Max",
+        value=df_describe.loc["quantity"].loc["max"],
+        delta=round(df_describe.loc["quantity"].loc["max"] - df_describe_pre.loc["quantity"].loc["max"], 2),
+    )
+
+# plot 2
+tab1, tab2, tab3 = st.tabs(["Sales", "Quantity", "Profit"])
+with tab1:
+    df_sales_by_month = df[df["order_date"].dt.year == year_filter]
+    df_sales_by_month['month_year'] = df_sales_by_month['order_date'].dt.strftime('%Y-%m')
+    sales_by_month = df_sales_by_month.groupby('month_year')['sales'].sum().reset_index()
+    fig = go.Figure(data=go.Scatter(x=sales_by_month['month_year'], y=sales_by_month['sales'], mode='lines', marker=dict(color='green')))
+    fig.update_layout(
+        title='Sales by Month',
+        xaxis=dict(title='Month'),
+        yaxis=dict(title='Sales'),
+        xaxis_tickangle=-45,
+        xaxis_tickfont=dict(size=8)
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+with tab2:
+    df_sales_by_month = df[df["order_date"].dt.year == year_filter]
+    df_sales_by_month['month_year'] = df_sales_by_month['order_date'].dt.strftime('%Y-%m')
+    sales_by_month = df_sales_by_month.groupby('month_year')['quantity'].sum().reset_index()
+    fig = go.Figure(data=go.Scatter(x=sales_by_month['month_year'], y=sales_by_month['quantity'], mode='lines', marker=dict(color='blue')))
+    fig.update_layout(
+        title='Sales by Month',
+        xaxis=dict(title='Month'),
+        yaxis=dict(title='Sales'),
+        xaxis_tickangle=-45,
+        xaxis_tickfont=dict(size=8)
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+with tab3:
+    df_sales_by_month = df[df["order_date"].dt.year == year_filter]
+    df_sales_by_month['month_year'] = df_sales_by_month['order_date'].dt.strftime('%Y-%m')
+    sales_by_month = df_sales_by_month.groupby('month_year')['profit'].sum().reset_index()
+    fig = go.Figure(data=go.Scatter(x=sales_by_month['month_year'], y=sales_by_month['profit'], mode='lines', marker=dict(color='purple')))
+    fig.update_layout(
+        title='Sales by Month',
+        xaxis=dict(title='Month'),
+        yaxis=dict(title='Sales'),
+        xaxis_tickangle=-45,
+        xaxis_tickfont=dict(size=8)
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+
+# country
+tab1, tab2, tab3 = st.tabs(["Sales", "Quantity", "Profit"])
+with tab1:
+    filtered_df = df[df['order_date'].dt.year == year_filter]
+    grouped_df = filtered_df.groupby('country')['sales'].sum()
+    top_10_countries = grouped_df.nlargest(10)
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=top_10_countries.index,
+        y=top_10_countries.values,
+        marker=dict(color=px.colors.qualitative.Plotly),  # Using Plotly's qualitative color palette
+    ))
+    fig.update_layout(
+        title='Top 10 Countries with Highest Profit',
+        xaxis=dict(title='Country'),
+        yaxis=dict(title='Profit'),
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+with tab2:
+    filtered_df = df[df['order_date'].dt.year == year_filter]
+    grouped_df = filtered_df.groupby('country')['quantity'].sum()
+    top_10_countries = grouped_df.nlargest(10)
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=top_10_countries.index,
+        y=top_10_countries.values,
+        marker=dict(color=px.colors.qualitative.Plotly),  # Using Plotly's qualitative color palette
+    ))
+    fig.update_layout(
+        title='Top 10 Countries with Highest Quantity',
+        xaxis=dict(title='Country'),
+        yaxis=dict(title='Quantity'),
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+with tab3:
+    filtered_df = df[df['order_date'].dt.year == year_filter]
+    grouped_df = filtered_df.groupby('country')['profit'].sum()
+    top_10_countries = grouped_df.nlargest(10)
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=top_10_countries.index,
+        y=top_10_countries.values,
+        marker=dict(color=px.colors.qualitative.Plotly),  # Using Plotly's qualitative color palette
+    ))
+    fig.update_layout(
+        title='Top 10 Countries with Highest Profit',
+        xaxis=dict(title='Country'),
+        yaxis=dict(title='Profit'),
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+
+# plot 3
+
+st.markdown("#### 2. Các nền tảng học tập nào được các kỹ sư và các học sinh tin dùng nhất?")
+
+df_market_pie = df.groupby(df['market'])['order_id'].count()
+fig_col1, fig_col2 = st.columns([5, 5])
+with fig_col1:
+    category_by_profit = df.groupby(['category', 'sub_category']).sum()['profit'].reset_index()
+    category_by_profit.sort_values('profit', ascending=False, inplace=True)
+    fig = go.Figure()
+    for category in category_by_profit['category'].unique():
+        data = category_by_profit[category_by_profit['category'] == category]
+        fig.add_trace(go.Bar(
+            x=data['sub_category'],
+            y=data['profit'],
+            name=category
+        ))
+    fig.update_layout(
+        title='Profit by Sub-Category within Each Category',
+        xaxis=dict(title='Sub-Category'),
+        yaxis=dict(title='Profit'),
+        barmode='group',
+        legend_title='Category'
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+with fig_col2:
+    category_by_count = df.groupby(['category', 'sub_category']).count()['order_id'].reset_index()
+    category_by_count.sort_values('order_id', ascending=False, inplace=True)
+    fig = go.Figure()
+    for category in category_by_count['category'].unique():
+        data = category_by_count[category_by_count['category'] == category]
+        fig.add_trace(go.Bar(
+            x=data['sub_category'],
+            y=data['order_id'],
+            name=category
+        ))
+    fig.update_layout(
+        title='Order Count by Sub-Category within Each Category',
+        xaxis=dict(title='Sub-Category'),
+        yaxis=dict(title='Order Count'),
+        barmode='group',
+        legend_title='Category'
+    )
+    st.plotly_chart(fig,use_container_width=True,height=800)
+
+# plot 5
+st.markdown("### 3. Top 10")
+tab1, tab2, tab3 = st.tabs(["Top 10 sales product", "Top 10 quantity product", "Top 10 customers"])
+with tab1:
+    products_sales = pd.DataFrame(df.groupby('product_name').sum()['sales'])
+    products_sales = products_sales.nlargest(columns="sales", n=10)
+    st.dataframe(products_sales)
+with tab2:
+    products_by_quantity = pd.DataFrame(df.groupby('product_name').sum()['quantity'])
+    products_by_quantity_sorted = products_by_quantity.nlargest(columns="quantity", n=10)
+    st.dataframe(products_by_quantity_sorted)
+with tab3:
+    top_10_customers = pd.DataFrame(df.groupby('customer_name').count()["order_id"])
+    top_10_customers = top_10_customers.nlargest(columns="order_id", n=10)
+    st.dataframe(top_10_customers)
