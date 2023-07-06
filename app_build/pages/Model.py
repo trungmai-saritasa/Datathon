dca6f6fda2b1ad1de4f4f505c516b5e043729592
Trung Mai <trung.mai@saritasa.com>
2023-07-07 03:45:14 +0700

    Update dashboard

--- app_build/pages/Model.py
+++ app_build/pages/Model.py
@@ -266,17 +266,17 @@ fig.add_trace(go.Scatter(x=cleaned_df.index, y=cleaned_df['sales'], mode='lines'
 fig.update_layout(title='Sales in Time Series', xaxis_title='Time', yaxis_title='Sales')
 st.plotly_chart(fig)
 
-cv = RepeatedStratifiedKFold(n_splits=2, n_repeats=10)
-linear_grid = {'positive': [True,False], 
-                'fit_intercept': [True, False], 
-                'n_jobs': [1, -1]}
-linear = GridSearchCV( lr(), param_grid=linear_grid,
-                          cv=cv, 
-                          scoring='completeness_score', 
-                          verbose=0)
-linear.fit(X_train, y_train)
+# cv = RepeatedStratifiedKFold(n_splits=2, n_repeats=10)
+# linear_grid = {'positive': [True,False], 
+#                 'fit_intercept': [True, False], 
+#                 'n_jobs': [1, -1]}
+# linear = GridSearchCV( lr(), param_grid=linear_grid,
+#                           cv=cv, 
+#                           scoring='completeness_score', 
+#                           verbose=0)
+# linear.fit(X_train, y_train)
 
-model2 = lr(**linear.best_params_).fit(X_train, y_train)
+model2 = lr().fit(X_train, y_train)
 pred = model2.predict(X_test)
 result2 = pd.DataFrame(pred, columns=['sales'], index=X_test.index)
 st.write('MSE of biased model:', metrics.mean_squared_error(y_test, pred))
